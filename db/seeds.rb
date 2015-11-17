62e76f0dcde5beb732ed380f2b42e35df5203818
Edem <edem228@gmail.com>
2015-11-17 19:50:02 +0100

    Mise en place des seeds avec un  user is_admin= true

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(
+	[	
+		{ first_name: 'Syyrius', 
+			last_name: 'Jio', 
+			function: 'Administrateur', phone: '009393939', 
+			email: 'xx@xxxx.com', 
+			password: '11111111', 
+			is_admin: 'true'}
+	]
+)
+User.create(
+	[	
+		{ first_name: 'Boris', 
+			last_name: 'Li', 
+			function: 'Maire', phone: '009393939', 
+			email: 'xx@xxxx.com', 
+			password: '11111111', 
+			is_admin: 'false'}
+	]
+)
+User.create(
+	[	
+		{ first_name: 'Henri', 
+			last_name: 'Li', 
+			function: 'Deputé', phone: '009393939', 
+			email: 'xx@xxxx.com', 
+			password: '11111111', 
+			is_admin: 'false'}
+	]
+)
