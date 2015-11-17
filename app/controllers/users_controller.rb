09d2ae24ddc504be8111934596df8aac25bd40f4
Edem <edem228@gmail.com>
2015-11-17 21:01:48 +0100

    Mise en place du login/logout show du user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+	def index
+		@user = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+				redirect_to root_path, notice: "\#{@user.first_name}\" a été modifié avec succés"
+		else
+			render 'edit', notice: "\#{@user.first_name}\" n'a pas été modifié"
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :function, :phone, :email, :password)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'users#index'
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
