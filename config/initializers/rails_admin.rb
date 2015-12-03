1968f3e22bed64fd2bac281ffc5fd4637964ed53
Edem <edem228@gmail.com>
2015-12-03 15:31:35 +0100

    Bouton de connexion dans rails admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,15 +1,16 @@
 require Rails.root.join('lib', 'rails_admin_publish.rb')
 RailsAdmin.config do |config|
   config.authorize_with do
-    redirect_to root_path unless current_user.is_admin == true
+    redirect_to root_path unless current_user.is_admin?
   end
   ### Popular gems integration
+
 
   # == Devise ==
   config.authenticate_with do
     warden.authenticate! scope: :user
   end
-  # config.current_user_method(&:current_user)
+  config.current_user_method(&:current_user)
 
   ## == Cancan ==
   # config.authorize_with :cancan
