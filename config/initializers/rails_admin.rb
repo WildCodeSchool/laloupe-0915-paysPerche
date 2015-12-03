7b01d3dc34e9932ae36519807369b589b0d3c9e8
Edem <edem228@gmail.com>
2015-12-03 10:29:15 +0100

    Rails Admin: creating a custom action

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,3 +1,4 @@
+require Rails.root.join('lib', 'rails_admin_publish.rb')
 RailsAdmin.config do |config|
   config.authorize_with do
     redirect_to root_path unless current_user.is_admin == true
@@ -21,19 +22,55 @@ RailsAdmin.config do |config|
 
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
 
-  config.actions do
-    dashboard                     # mandatory
-    index                         # mandatory
-    new
-    export
-    bulk_delete
-    show
-    edit
-    delete
-    show_in_app
+  # config.actions do
+  #   dashboard                     # mandatory
+  #   index                         # mandatory
+  #   new
+  #   export
+  #   bulk_delete
+  #   show
+  #   edit
+  #   delete
+  #   show_in_app
 
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
+  #   ## With an audit adapter, you can add:
+  #   # history_index
+  #   # history_show
+  # end
+
+  # Register the class in lib/rails_admin_publish.rb
+module RailsAdmin
+  module Config
+    module Actions
+      class Publish < RailsAdmin::Config::Actions::Base
+        RailsAdmin::Config::Actions.register(self)
+      end
+    end
   end
+end
+ 
+config.actions do
+  # root actions
+  dashboard                     # mandatory
+  # collection actions
+  index                         # mandatory
+  new
+  export
+  history_index
+  bulk_delete
+  # member actions
+  show
+  edit
+  delete
+  history_show
+  show_in_app
+ 
+  # Set the custom action here
+  publish do
+    # Make it visible only for article model. You can remove this if you don't need.
+    visible do
+      bindings[:abstract_model].model.to_s == "Article"
+    end
+  end
+end
 end
