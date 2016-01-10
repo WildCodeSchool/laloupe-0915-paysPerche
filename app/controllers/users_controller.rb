5c23b75e4ca79d4d7e3bb20a22420ca1ff8b06ad
Edem <edem228@gmail.com>
2016-01-10 19:00:36 +0100

    Après envoi du mail groupé, redirection[provisoire]  vers le root_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,10 +36,10 @@ class UsersController < ApplicationController
 		users = User.group_to_send(params).flatten!
 		message = User.message_to_send(params)
 		subject = User.email_subject(params)
-		binding.pry
 		users.map do |user|
 			ContactMailer.mail_groupe(user, subject, message).deliver_later!
 		end
+		redirect_to root_path, notice: "Mail bien envoyé"
 		
 	end
 
