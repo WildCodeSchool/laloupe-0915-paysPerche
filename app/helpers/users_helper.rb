06dbfe647699e02679b1ac8627d9944fc2ee0671
Edem <edem228@gmail.com>
2016-01-20 16:34:29 +0100

    Seuls les utilisateurs ayant le droit is_admin? = true sont autorisés à envoyer un mail groupé. Si non redirect_to root_path

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,4 @@
 module UsersHelper
-  def is_admin
-    @user = current_user
-    if @user.is_admin == true
-      redirect_to rails_admin_path
-    end
-  end
   
   def group_to_send(params)
     tab_users = []
