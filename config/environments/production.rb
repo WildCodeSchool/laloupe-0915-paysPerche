164d0b98f176362848ef0ca65feda6d089ff25b2
Karb0 <carbonel.mathieu@gmail.com>
2016-02-09 21:13:47 +0100

    config smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,13 +75,14 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address        => "smtp.gmail.com",
-                :port           => 465,
-                :authentication => :plain,
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => ENV['GMAIL_USERNAME'],
+      :password             => ENV['GMAIL_PASSWORD'],
+      :authentication       => :plain,
+      :enable_starttls_auto => true}
+  
   config.action_mailer.default_url_options = { host: ENV['PRODUCTION_HOST']}
 
   # Do not dump schema after migrations.
